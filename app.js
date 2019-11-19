e465071966288baf9effcf66af15ce3425a8b72b
Nathan Kloer <nathan.kloer@gmail.com>
2019-11-19 12:37:41 -0500

    made the quotient and remainder appear onscreen w/o use of console

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // program quotient answer
 
 document.getElementById("Btn").addEventListener("click", submit);
-function submit() {  
+function submit() {
     var in1 = document.getElementById("inputOne").value;
     var in2 = document.getElementById("inputTwo").value;
     var x = in1 - in2;
@@ -11,36 +11,13 @@ function submit() {
         document.getElementById("demo").innerHTML = "The First Number must be larger than the Second Number!";
         return;
     }
-    for (var i = 0; x > 0; i++ ) {
+    for (var i = 0; x > 0; i++) {
         x = x - in2;
     }
     var result = i;
-    // var remainder = Number(in2) + x;
-    // if (x = 0) {
-    //     remainder = 0;
-    // } else { remainder = Number(in2) + x }
-    if ( x == 0 ) {
-        console.log("the answer is " + (result + 1));
-        console.log("There is no Remainder");
+    if (x == 0) {
+        document.getElementById("machine").append("The Quotient is "+ (result + 1) + ". And there is no Remainder");
     } else {
-    console.log("the answer is " + result);
-    console.log("The Remainder is " + (Number(in2)+x));
+        document.getElementById("machine").append("The Quotient is " + result + ". And the Remainder is " + (Number(in2) + x));
     }
 }
-
-// document.getElementById("demo").innerHTML = x;
-
-
-// function myFunction(){console.log("hello")};
-// Object.onclick 
-// function myFunction() {
-//     document.getElementById("machine").submit();
-//   }
-
-// var userInput = [inputOne, inputTwo]
-
-
-
-// for (var i = 0; i > 10; i++) {
-//     document.getElementbyID
-// }
